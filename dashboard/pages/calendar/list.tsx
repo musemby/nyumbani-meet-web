5db0fdbcfe5c9bb60b0403cce1ac16d0876fe522
Paul Onteri <onteripaul@gmail.com>
2024-07-30 14:54:21 +0300

    calendar list

---
+++ dashboard/pages/calendar/list.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Table, Tag } from "antd";
+import { useUserList } from "../../src/api-client/user";
+
+const Users = () => {
+  const {
+    data: users,
+    isLoading: usersIsLoading,
+    // isError: usersIsError,
+    // refetch: refetchUsers,
+  } = useUserList();
+
+  return (
+    <>
+      <Table
+        style={{
+          margin: "20px auto",
+        }}
+        dataSource={users || []}
+        bordered
+        title={() => "Tenants"}
+        loading={usersIsLoading}
+        columns={[
+          {
+            title: "Name",
+            dataIndex: "name",
+            key: "name",
+          },
+          {
+            title: "House Number",
+            dataIndex: "house_number",
+            key: "house_number",
+          },
+          {
+            title: "Phone",
+            dataIndex: "phone_number",
+            key: "phone_number",
+          },
+          {
+            title: "Admin",
+            dataIndex: "is_admin",
+            render: (text) =>
+              text ? (
+                <Tag color={"green"}>Yes</Tag>
+              ) : (
+                <Tag color={"red"}>No</Tag>
+              ),
+          },
+        ]}
+      />
+    </>
+  );
+};
+
+export default Users;
