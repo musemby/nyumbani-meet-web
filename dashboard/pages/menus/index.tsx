5cf7da99215ec922176b8ae3160c30f57b2ca315
Paul Onteri <onteripaul@gmail.com>
2024-08-03 12:11:40 +0300

    menu check if admin

--- dashboard/pages/menus/index.tsx
+++ dashboard/pages/menus/index.tsx
@@ -4,6 +4,7 @@ import { useMenuList, useActivateMenu } from "../../src/api-client/menus";
 import { SERVER_URL } from "../../src/constants";
 import { LinkOutlined } from "@ant-design/icons";
 import { useSnackbar } from "notistack";
+import { useUser } from "../../src/api-client/user";
 
 const Menus = () => {
   const snackbar = useSnackbar();
@@ -13,6 +14,8 @@ const Menus = () => {
     // isError: menusIsError,
     refetch: refetchMenus,
   } = useMenuList();
+
+  const { data: user } = useUser("me");
 
   const { mutateAsync: activateMenu } = useActivateMenu();
 
@@ -65,28 +68,32 @@ const Menus = () => {
               );
             },
           },
-          {
-            title: "Activate",
-            dataIndex: "id",
-            key: "id",
-            render: (id: string, row) => {
-              return (
-                <>
-                  {row.is_active_menu ? (
-                    <Tag color={"green"}>Active</Tag>
-                  ) : (
-                    <Button
-                      type="primary"
-                      onClick={() => handleActivateMenu(id)}
-                      disabled={menusIsLoading}
-                    >
-                      Activate
-                    </Button>
-                  )}
-                </>
-              );
-            },
-          },
+          ...(user && user.is_admin
+            ? [
+                {
+                  title: "Activate",
+                  dataIndex: "id",
+                  key: "id",
+                  render: (id: string, row) => {
+                    return (
+                      <>
+                        {row.is_active_menu ? (
+                          <Tag color={"green"}>Active</Tag>
+                        ) : (
+                          <Button
+                            type="primary"
+                            onClick={() => handleActivateMenu(id)}
+                            disabled={menusIsLoading}
+                          >
+                            Activate
+                          </Button>
+                        )}
+                      </>
+                    );
+                  },
+                },
+              ]
+            : []),
         ]}
       />
     </>
